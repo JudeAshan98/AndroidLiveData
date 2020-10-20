f38de47c9c03fcaa90db1662eec7c9108f82805a
JudeAshan98 <judeashanlakmal356@gmail.com>
2020-10-20 16:11:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/livedata/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.livedata;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView) findViewById(R.id.list);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.VISIBLE);
+        MainActivityViewModel model = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        model.getFruitList().observe(this, fruitlist -> {
+            // update UI
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                    android.R.layout.simple_list_item_1, android.R.id.text1, fruitlist);
+            // Assign adapter to ListView
+            listView.setAdapter(adapter);
+            progressBar.setVisibility(View.GONE);
+        });
+    }
+}
